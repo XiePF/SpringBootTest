6ed8001255c1c3101bf4b4b19adb8a1565c464bd
xiepf <xiepf.sky@gmail.com>
2017-09-23 16:53:21 +0800

    test

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Descripition:
+ * @Author: xiepf
+ * @Date: 2017/9/23 16:52
+ */
+@RestController("/test")
+public class Test {
+
+    @RequestMapping("/hi")
+    public  String hello()
+    {
+        return  "hello world";
+    }
+}
